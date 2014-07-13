896a844e3990af900cc5d2d7edf3586eecfb6d67
dany1468 <dany1468@gmail.com>
2014-07-13 18:29:31 +0900

    Add pulse-width modulation sample

---
+++ LedPwmControl/LedPwmControl.ino
@@ -0,0 +1,21 @@
+const int LED = 9;
+
+int i = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (i = 0; i < 255; i++) {
+    analogWrite(LED, i);
+    delay(10);
+  }
+  
+  for (i = 255; i > 0; i--) {
+    analogWrite(LED, i);
+    delay(10);
+  }
+}
